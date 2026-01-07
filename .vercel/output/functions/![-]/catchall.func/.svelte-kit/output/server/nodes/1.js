9bbcaf0f8d2ac3216ea0b17fc42a57598744a32a
WaleedMilk <137730899+WaleedMilk@users.noreply.github.com>
2026-01-06 21:21:50 -0700

    Remove broken Vercel output, add gitignore
    - Delete incomplete .vercel/output that was causing build errors
    - Add .gitignore to prevent build artifacts from being committed
    - Vercel will now build from source on deploy

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.UQR5zE-y.js","_app/immutable/chunks/CaaXtaQm.js","_app/immutable/chunks/ClOfLx3j.js","_app/immutable/chunks/DNpArFlC.js","_app/immutable/chunks/CNs9amC5.js","_app/immutable/chunks/B_EtmAjJ.js","_app/immutable/chunks/Dz_rfxTK.js","_app/immutable/chunks/DhUVGaAf.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/surah/_id_/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.TKZIVAwz.js","_app/immutable/chunks/CaaXtaQm.js","_app/immutable/chunks/ClOfLx3j.js","_app/immutable/chunks/BT6qVgAc.js","_app/immutable/chunks/Rgw4rWUl.js","_app/immutable/chunks/BDZE3697.js","_app/immutable/chunks/CfuIVgKi.js","_app/immutable/chunks/Dz_rfxTK.js","_app/immutable/chunks/B_EtmAjJ.js","_app/immutable/chunks/DhUVGaAf.js","_app/immutable/chunks/CNs9amC5.js","_app/immutable/chunks/pK6643e_.js","_app/immutable/chunks/BzEnRNjs.js"];
-export const stylesheets = [];
-export const fonts = [];
